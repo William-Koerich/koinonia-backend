3f6843cdaffb4156e6b5d59e40617df61f793872
William Koerich <williamkoerich17@gmail.com>
2026-06-29 16:33:29 -0300

    adiciona interface UpdateUsuarioInput para atualização de informações do usuário

--- src/modules/usuario/service/update-usuario.ts
+++ src/modules/usuario/service/update-usuario.ts
@@ -1,7 +1,23 @@
-import { UpdateUsuarioDTO, UsuarioRepository } from "../repository/usuario";
+import { TipoUsuario, Genero, EstadoCivil } from "@prisma/client";
+import { UsuarioRepository } from "../repository/usuario";
+
+export interface UpdateUsuarioInput {
+  nome?: string;
+  sobrenome?: string;
+  email?: string;
+  foto?: string;
+  tipo?: TipoUsuario;
+  ativo?: boolean;
+  dataAniversario?: string;
+  genero?: Genero;
+  estadoCivil?: EstadoCivil;
+  logradouro?: string;
+  bairro?: string;
+  cidade?: string;
+}
 
 export class UpdateUsuarioService {
-  async execute(id: string, data: UpdateUsuarioDTO) {
+  async execute(id: string, data: UpdateUsuarioInput) {
     const repository = new UsuarioRepository();
     const exists = await repository.findById(id);
     if (!exists) throw new Error("Usuário não encontrado");
